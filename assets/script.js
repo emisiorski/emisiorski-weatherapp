95ca71d8cff4f3e68a6724d1100e3493ffae80ff
Erica <ericamisiorski@gmail.com>
2021-12-07 18:49:17 -0500

    adding java api

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var apiKey = "&cnt=5&appid=10f55498923b2e73e6958bb7868b041c";
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=";
+var exclude = "&exclude=minutely,hourly,alerts&units=imperial";
+
+var cityList = [];
+var counter = 0;
+
+
+// need to save the search when clicking the submit city button
+ function saveCity (city) {
+
+ }
+
+
+// need to display the weather forecast at the top
+
+
+
+// 
