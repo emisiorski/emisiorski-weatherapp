5e872fd73908fa7fb7ce7861d2c4e36effea5148
Erica <ericamisiorski@gmail.com>
2021-12-28 22:05:40 -0500

    search button function

--- assets/script.js
+++ assets/script.js
@@ -5,16 +5,31 @@ var exclude = "&exclude=minutely,hourly,alerts&units=imperial";
 
 var cityList = [];
 var counter = 0;
-
+var searchButton = document.getElementById("search-button");
+var cities = document.querySelector("#saved-cities");
 
 // need to save the search when clicking the submit city button
- function saveCity (city) {
+function saveCities(city) {
+   var searchedCity = document.createElement("button");
+   searchedCity.setAttribute("id", city);
+   searchedCity.className = "city-button";
+   searchedCity.textContent = city;
+   // searchedCity.addEventListener("click", function () {
+   //    displayCity(this.id);
+   // });
+   cities.appendChild(searchedCity);
+   cityList.push(city);
+   localStorage.setItem("cityList", JSON.stringify(cityList));
+   counter++;
+}
 
- }
 
+// // // need to display the weather forecast at the top
 
-// need to display the weather forecast at the top
-
-
-
-// 
+searchButton.addEventListener("click", function () {
+   var cityName = document.getElementById("the-city").value;
+   // displayCity(cityName);
+   saveCities(cityName);
+   console.log(cityName);
+});
+// // //
